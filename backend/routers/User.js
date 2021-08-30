4b8aeb7168e6d0eb8dd88975d896d04fab0a447d
bahoang3105 <bahoang3105@gmail.com>
2021-08-30 17:47:42 +0700

    Update User.js

--- backend/routers/User.js
+++ backend/routers/User.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const controller = require("../controllers/authController");
 
-router.route("/signup").put(controller.signup);
+router.route("/signup").post(controller.signup);
 router.route("/login").post(controller.login);
 
 module.exports = router;
